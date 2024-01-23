0c111bc7ecc0eb54c3856b64ec4652f7828af9a2
Edson Barreto <edson.barreto@outlook.com>
2024-01-23 20:43:53 -0300

    refactory remove import sem uso

--- src/main/java/br/com/adoptpet/config/pet/InserirPetConfig.java
+++ src/main/java/br/com/adoptpet/config/pet/InserirPetConfig.java
@@ -1,8 +1,6 @@
 package br.com.adoptpet.config.pet;
 
-import br.com.adoptpet.core.usecase.endereco.impl.InserirEnderecoUseCaseImpl;
 import br.com.adoptpet.core.usecase.pet.impl.InserirPetUseCaseImpl;
-import br.com.adoptpet.dataprovider.InserirEnderecoImpl;
 import br.com.adoptpet.dataprovider.InserirPetImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
